0cacf8a309c045ce8f4afb1cba05849ab5df14d2
Luis Granados <granal88@gmail.com>
2019-02-01 18:00:05 -0500

    added end of game mechanic

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings():
 		
 		# Ship settings
 		self.ship_speed_factor = 10.5
-		self.ship_limit = 3
+		self.ship_limit = 1
 
 		# Bullet settings
 		self.bullet_speed_factor = 10
@@ -20,7 +20,7 @@ class Settings():
 		self.bullets_allowed = 100
 
 		# Alien settings
-		self.alien_speed_factor = 10
+		self.alien_speed_factor = 50
 		self.fleet_drop_speed = 10
 		# fleet_direction of 1 represents right; -1 represents left.
 		self.fleet_direction = 1 
