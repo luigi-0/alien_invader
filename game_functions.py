3ec1569f3d8d761d68cea530559b74d7a31bc486
Luis Granados <granal88@gmail.com>
2019-01-31 23:18:29 -0500

    allow bullet collisions

--- game_functions.py
+++ game_functions.py
@@ -54,7 +54,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 	# Make the most recently drawn screen visible.
 	pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
 	"""Update position of bullets and get rid of old bullets."""
 	# Update bullet positions.
 	bullets.update()
@@ -63,6 +63,10 @@ def update_bullets(bullets):
 	for bullet in bullets.copy():
 		if bullet.rect.bottom <= 0:
 			bullets.remove(bullet)
+
+	# Check for any bullets that have hit aliens.
+	# If so, get rid of the bullet and the alien.
+	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 def create_fleet(ai_settings, screen, ship, aliens):
 	"""Create a full fleet of aliens."""
