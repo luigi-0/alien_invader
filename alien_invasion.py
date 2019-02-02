880a5f0b77049a4f2bb3bbf5420b07cddc2c3ae4
Luis Granados <granal88@gmail.com>
2019-02-02 13:46:17 -0500

    add play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 from alien import Alien
 from game_stats import GameStats
+from button import Button
 import game_functions as gf
 
 
@@ -14,6 +15,9 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien_Invasion")
+
+	# Make the play button.
+	play_button = Button(ai_settings, screen, "Play")
 
 	# Create an instance to store game statistics.
 	stats = GameStats(ai_settings) 
@@ -28,12 +32,12 @@ def run_game():
 
 	# Start the main loop for the game.
 	while True:
-		gf.check_events(ai_settings, screen, ship, bullets)
+		gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
 		if stats.game_active:
 			ship.update()
 			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
 			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 			
-		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+		gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 		
 run_game()
