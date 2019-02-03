1a9d111fe3a977f8e878863d867c1524e206f1fb
Luis Granados <granal88@gmail.com>
2019-02-03 10:56:50 -0500

    add and display levels

--- game_functions.py
+++ game_functions.py
@@ -29,7 +29,7 @@ def check_keyup_events(event, ship):
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left = False
 	
-def check_events(ai_settings, screen, stats, play_button, ship, aliens,
+def check_events(ai_settings, screen, stats, sb, play_button, ship, aliens,
 		bullets):
 	"""Respond to keypresses and mouse events."""
 	for event in pygame.event.get():
@@ -38,8 +38,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens,
 
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			mouse_x, mouse_y = pygame.mouse.get_pos()
-			check_play_button(ai_settings, screen, stats, play_button, ship,
-				aliens, bullets, mouse_x, mouse_y)
+			check_play_button(ai_settings, screen, stats, sb, play_button,
+				ship, aliens, bullets, mouse_x, mouse_y)
 
 		elif event.type == pygame.KEYDOWN:
 			check_keydown_events(event, ai_settings, screen, ship, bullets)
@@ -47,13 +47,18 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens,
 		elif event.type == pygame.KEYUP:
 			check_keyup_events(event, ship)
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
-	bullets, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, sb, play_button, ship,
+	aliens, bullets, mouse_x, mouse_y):
 	"""Start a new game when the player clicks Play."""
 	button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
 	if button_clicked and not stats.game_active:
 		# Reset the game settings.
 		ai_settings.initialize_dynamic_settings()
+
+		# Reset the scoreboard images.
+		sb.prep_score()
+		sb.prep_high_score()
+		sb.prep_level()
 
 		# Hide the mouse cursor.
 		pygame.mouse.set_visible(False)
@@ -119,12 +124,16 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
 		check_high_score(stats, sb)
 
 	if len(aliens) == 0:
-		# Destory existing bullets, speed up game, and create new fleet.
+		# If the entire fleet is destroyed, start a new level.
 		bullets.empty()
 		ai_settings.increase_speed()
+
+		# Increase level.
+		stats.level += 1
+		sb.prep_level()
+
 		create_fleet(ai_settings, screen, ship, aliens)
 	
-
 def create_fleet(ai_settings, screen, ship, aliens):
 	"""Create a full fleet of aliens."""
 	# Create an alien and find the number of aliens in a row.
